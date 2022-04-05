126d7914996c3111931fafe4a89d6d5c22020050
Matthias Osswald <m@osswald.li>
2022-04-05 20:46:15 +0200

    Refactor cli interface.

--- facs_analysis/config.py
+++ facs_analysis/config.py
@@ -2,17 +2,19 @@ from pathlib import Path
 
 
 class Config:
-    GATING = Path("./Table_BM_Trumpp.csv")
     CELL_COUNTS = Path("./cell_counts.csv")
     OUTPUT_DIR = Path.cwd()
     start_pop = "coi/db/lin-"
     order = ["B", "S"]
     organ = "BM lin-"
     power = "6"
-    part_to_remove = "_1_001.fcs"
     stat_ind = "yes"
     single_comp = "no"
     control_group = "BM_all"
     stat_test = "t-test_welch"
     datatype = "png"
     size = 300
+
+    def __init__(self, gating_path, part_to_remove):
+        self.GATING = Path(gating_path)
+        self.part_to_remove = part_to_remove
